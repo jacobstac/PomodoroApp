d494abc482b6f86a4d19e609b8a00db9ae05af6c
stac <jacob.benjamin.stachowicz@gmail.com>
2018-04-18 00:28:50 +0200

    rendered the pomos with the help of a conponent level state

--- Pomodoro/src/components/PomoList.js
+++ Pomodoro/src/components/PomoList.js
@@ -3,21 +3,40 @@ import { View, Text } from 'react-native';
 import axios from 'axios';
 
 class PomoList extends Component {
+
+    // class level property
+    state = { pomos: []} // initial base/empty state
+
     /* componentWillMount utförs is samma stund som PomoList ska läggas på skärmen.
     Detta är bra när man t.ex ska hämta data */
     componentWillMount () {
-        console.log('tjenare');
+        axios.get('https://raw.githubusercontent.com/jacobstac/PomodoroApp/albums/Pomodoro/src/pomos.json')     //axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+            .then(response => this.setState({ pomos: response.data }));
+    }
+
+    renderPomos() {
+        return this.state.pomos.map(pomos => <Text>{ pomos.title }</Text>)
     }
 
     render() {
+        console.log(this.state)
         return (
-            <View>
-                <Text>Ämnen!</Text>
+            <View Style = {pomoStyle}>
+                { this.renderPomos() }
             </View>
         );
     }
+
+    const styles = {
+        pomoStyle : {
+            justifyContent: 'center'
+        }
+    };
+
 }
 
+
 export default PomoList
+
 
 
