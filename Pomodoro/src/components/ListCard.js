f565c04eaa4e01d2ea23c6cb8ad9ef34d6ee075b
stac <jacob.benjamin.stachowicz@gmail.com>
2018-04-22 16:58:09 +0200

    added pomodoro image

--- Pomodoro/src/components/ListCard.js
+++ Pomodoro/src/components/ListCard.js
@@ -3,13 +3,16 @@ import { Text, View, Image } from 'react-native';
 import styles from '../styles/Styles.js'
 
 const ListCard = (props) => {
+    
     const { textStyle24, viewStyle, cardContainerStyle } = styles;
     return (
     <View style = { cardContainerStyle }>
         <Text style = { textStyle24 } >{props.toListCard.title} </Text>
         <Text >Total: {props.toListCard.total} </Text>
         <Text >Today: {props.toListCard.today} </Text>
-        <Image></Image>
+        <Image style={{width: 50, height: 50}}
+          source={{uri: 'https://raw.githubusercontent.com/jacobstac/PomodoroApp/dynamic/pic.png'}}
+        />
 
     </View>
     );
